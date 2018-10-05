705e32671a46cd8116caf2e3cdc0a486fd561b0c
cbrooks42 <43892026+cbrooks42@users.noreply.github.com>
2018-10-05 16:19:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     install_requires=['web3'],
     version='0.1',
     license='GPL-3.0',
+    py_modules=['which_block'],
     description='Accepts a contract address and a web3 host, quickly traverses the block chain, and returns the block hash and transaction hash in which the contract was deployed.',
     long_description=readfile('README.md'),
     entry_points={
