f69df87e91f5148891fe0f0cd0394cacc2757603
cbrooks42 <43892026+cbrooks42@users.noreply.github.com>
2018-10-08 15:13:46 -0400

    Removing long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
 
-def readfile(filename):
-    with open(filename, 'r+') as f:
-        return f.read()
-    
 setup(
     name='which_block',
     url='https://github.com/cbrooks42/which_block',
@@ -14,10 +10,9 @@ setup(
     license='GPL-3.0',
     py_modules=['which_block'],
     description='Accepts a contract address and a web3 host, quickly traverses the block chain, and returns the block hash and transaction hash in which the contract was deployed.',
-    long_description=readfile('README.md'),
     entry_points = {
         'console_scripts': [
-            'which-block=which_block.which_block:main',
+            'which-block=which_block:main',
         ],
     },
 )
